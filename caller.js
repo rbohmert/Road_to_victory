041206eb436ee722df7fe49d5daf845010468014
Joks <jbateau@student.42.fr>
2017-04-08 20:09:14 +0200

    Img:Asterix

--- caller.js
+++ caller.js
@@ -6,7 +6,7 @@ script.onload = function () {
             title: "malyari",                          // Sert a rien
             x: 693,                                   // Coordonnées X du départ de l'image (coin supérieur gauche)
             y: 526,                                  // Coordonnées Y du départ de l'image (coin supérieur gauche)
-            image: "http://img4.hostingpics.net/pics/559724melenchon.png" // Pour changer l'image du script il faut remplacer cette adresse par l'adresse d'un png qui respecte un certain format
+            image: "http://img4.hostingpics.net/pics/822593Asterix.png" // Pour changer l'image du script il faut remplacer cette adresse par l'adresse d'un png qui respecte un certain format
         }
     ];
  
