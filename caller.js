f0afb570119207e0a74526042536c8353b500a80
jbatea <jbateau@student.42.fr>
2017-04-09 18:17:00 +0300

    Update caller.js

--- caller.js
+++ caller.js
@@ -4,8 +4,8 @@ script.onload = function () {
     var images = [
         {
             title: "malyari",                          // Sert a rien
-            x: 693,                                   // Coordonnées X du départ de l'image (coin supérieur gauche)
-            y: 526,                                  // Coordonnées Y du départ de l'image (coin supérieur gauche)
+            x: 63,                                   // Coordonnées X du départ de l'image (coin supérieur gauche)
+            y: 846,                                  // Coordonnées Y du départ de l'image (coin supérieur gauche)
             image: "http://img4.hostingpics.net/pics/822593Asterix.png" // Pour changer l'image du script il faut remplacer cette adresse par l'adresse d'un png qui respecte un certain format
         }
     ];
