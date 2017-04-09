c36d999d200f8fb26c7fa4e6ac42db030ae32d85
jbatea <jbateau@student.42.fr>
2017-04-09 19:57:48 +0300

    Update autoscript.js

--- autoscript.js
+++ autoscript.js
@@ -174,30 +174,38 @@ drawImage: drawImage,
 		painters[i] = Painter(images[i]);
 	}
 
- 	function draw(){
-  		console.log("DRAW");
-  			for(var i = 0; i < painters.length; i++){
-  				if(painters[i].isReady()){
-  					var result = painters[i].drawImage();
-  
-  					if(result > 0){
- +						console.log("result > 0");
- +						console.log("SET TIMEOUT result*1000");
-  						setTimeout(draw, result*1000);
-  						return;
-  					}
-  					else{
- +						console.log("result < 0");
-  						continue;
-  					}
-  				}
-  				else{
-  					continue;
-  				}
-  			}
- +			console.log("SET TIEMOUT 3000");
-  			setTimeout(draw, 3000);
-  		return;
-  	}
-  	draw();
- }
+	function draw(){
+		console.log("DRAW");
+		//var timer = (App.cooldown-(new Date).getTime())/1E3;
+		var timer = lastPlaceTime - new Date().getTime() + cooldown;
+		if(0<timer){
+			console.log("TIMER > 0");
+			console.log("timer: " + timer);
+			setTimeout(draw, 1000);
+		}
+		else{
+			console.log("Timer < 0");
+			for(var i = 0; i < painters.length; i++){
+				if(painters[i].isReady()){
+					var result = painters[i].drawImage();
+
+					if(result > 0){
+						setTimeout(draw, result*1000);
+						return;
+					}
+					else{
+						continue;
+					}
+				}
+				else{
+					console.log("painter isnt ready");
+					continue;
+				}
+			}
+			setTimeout(draw, 3000);
+		}
+
+		return;
+	}
+	draw();
+}
